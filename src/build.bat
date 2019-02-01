198dc3f05061d5a31e84017c605351116bec5ca2
zerocjx <zerocjx@outlook.com>
2019-02-02 01:04:32 +0800

    更新：1.使用UTF-8格式读取源代码文件（处理源代码文件中的中文，例如注释。【warning C4819】）；2.如果有，使用UNICODE形式的API函数。

--- src/build.bat
+++ src/build.bat
@@ -3,7 +3,8 @@
 mkdir ..\build
 copy ..\third_party\SDL2\lib\x64\SDL2.dll ..\build
 pushd ..\build
-cl ..\src\main.cpp -I ..\third_party\SDL2\include\ SDL2.lib SDL2main.lib SDL2test.lib^
+cl ..\src\main.cpp ^
+    /utf-8 /D UNICODE -I ..\third_party\SDL2\include\ SDL2.lib SDL2main.lib SDL2test.lib^
     /link /ENTRY:WinMainCRTStartup /SUBSYSTEM:WINDOWS /LIBPATH:..\third_party\SDL2\lib\x64\
 popd
 
