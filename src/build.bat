1e9c84b4331fa8ec732a14aba1e793150712a281
zerocjx <zerocjx@outlook.com>
2019-02-02 00:42:18 +0800

    更新：更新SDL2库的位置

--- src/build.bat
+++ src/build.bat
@@ -1,10 +1,10 @@
 @echo off
 
 mkdir ..\build
-copy ..\src\SDL2\lib\x64\SDL2.dll ..\build
+copy ..\third_party\SDL2\lib\x64\SDL2.dll ..\build
 pushd ..\build
-cl ..\src\main.cpp -I ..\src\SDL2\include\ SDL2.lib SDL2main.lib SDL2test.lib^
-    /link /ENTRY:WinMainCRTStartup /SUBSYSTEM:WINDOWS /LIBPATH:..\src\SDL2\lib\x64\
+cl ..\src\main.cpp -I ..\third_party\SDL2\include\ SDL2.lib SDL2main.lib SDL2test.lib^
+    /link /ENTRY:WinMainCRTStartup /SUBSYSTEM:WINDOWS /LIBPATH:..\third_party\SDL2\lib\x64\
 popd
 
 start ..\build\main.exe
