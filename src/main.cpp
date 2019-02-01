8fab17aa572be70135821a791236db7dc66e6f7d
zerocjx <zerocjx@outlook.com>
2019-02-02 01:06:12 +0800

    更新：添加中文，来测试中文在窗口标题中的显示是否正常。

--- src/main.cpp
+++ src/main.cpp
@@ -10,39 +10,41 @@ void render()
     // Get the window surface
     screen = SDL_GetWindowSurface(window);
 
-    // Lock surface if needed
-    if (SDL_MUSTLOCK(screen))
-    {
-        if (SDL_LockSurface(screen) < 0)
-        {
-            return;
-        }
-    }
+    // // Lock surface if needed
+    // if (SDL_MUSTLOCK(screen))
+    // {
+    //     if (SDL_LockSurface(screen) < 0)
+    //     {
+    //         return;
+    //     }
+    // }
 
 
-    // Ask SDL for the time in milliseconds
-    int tick = SDL_GetTicks();
+    // // Ask SDL for the time in milliseconds
+    // int tick = SDL_GetTicks();
 
-    // Declare a couple of variables
-    int i, j, yofs, ofs;
+    // // Declare a couple of variables
+    // int i, j, yofs, ofs;
 
-    // Draw to screen
-    yofs = 0;
-    for (int i = 0; i < 480; i++)
-    {
-        for (int j = 0, ofs = yofs; j < 640; j++, ofs++)
-        {
-            ((unsigned int*)screen->pixels)[ofs] = i * i + j * j + tick;
-        }
-        yofs += screen->pitch / 4;
-    }
+    // // Draw to screen
+    // yofs = 0;
+    // for (int i = 0; i < 480; i++)
+    // {
+    //     for (int j = 0, ofs = yofs; j < 640; j++, ofs++)
+    //     {
+    //         ((unsigned int*)screen->pixels)[ofs] = i * i + j * j + tick;
+    //     }
+    //     yofs += screen->pitch / 4;
+    // }
 
-    // Unlock if needed
-    if (SDL_MUSTLOCK(screen))
-    {
-        SDL_UnlockSurface(screen);
-    }    
+    // // Unlock if needed
+    // if (SDL_MUSTLOCK(screen))
+    // {
+    //     SDL_UnlockSurface(screen);
+    // }    
 
+    // 另外一种绘制方式
+    SDL_FillRect( screen, NULL, SDL_MapRGB( screen->format, 0x00, 0x00, 0xFF ) );
 
 
     // Tell SDL to update the whole screen
@@ -64,7 +66,7 @@ int main(int argc, char *argv[])
     atexit(SDL_Quit);
 
     // Attempt to create a 640*480 window with 32bit pixels.
-    window = SDL_CreateWindow("IMGUI - Learn", 
+    window = SDL_CreateWindow("IMGUI - Learn 窗口", 
                 SDL_WINDOWPOS_UNDEFINED, 
                 SDL_WINDOWPOS_UNDEFINED, 
                 640, 480,
