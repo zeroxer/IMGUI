de86e5187f07bfc001f4476b04ce520ba64ffcf3
zerocjx <zerocjx@outlook.com>
2019-02-02 21:51:50 +0800

    更新：更新build.bat的linkFlags格式，改成每个参数单独占据一行。

--- src/build.bat
+++ src/build.bat
@@ -5,7 +5,9 @@ set platform=x64
 set debugFlags=/Zi
 set encodingFlags=/utf-8 /D UNICODE
 set includeFlags=-I..\third_party\SDL2\include\
-set linkFlags=/link /ENTRY:WinMainCRTStartup /SUBSYSTEM:WINDOWS /LIBPATH:..\third_party\SDL2\lib\%platform%\
+set linkFlags=/link /ENTRY:WinMainCRTStartup ^
+                    /SUBSYSTEM:WINDOWS ^
+                    /LIBPATH:..\third_party\SDL2\lib\%platform%\
 set libFlags=SDL2.lib SDL2main.lib SDL2test.lib
 
 where cl > nul 2>&1 || call ..\misc\shell.bat
