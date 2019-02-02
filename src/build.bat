243f16c59570cca499019bfc0e35253029add4ab
zerocjx <zerocjx@outlook.com>
2019-02-02 21:46:23 +0800

    修复：修复where cl仍然输出错误信息的问题。(build.bat)

--- src/build.bat
+++ src/build.bat
@@ -8,7 +8,7 @@ set includeFlags=-I..\third_party\SDL2\include\
 set linkFlags=/link /ENTRY:WinMainCRTStartup /SUBSYSTEM:WINDOWS /LIBPATH:..\third_party\SDL2\lib\%platform%\
 set libFlags=SDL2.lib SDL2main.lib SDL2test.lib
 
-where cl >& 2> nul || call ..\misc\shell.bat
+where cl > nul 2>&1 || call ..\misc\shell.bat
 
 if not exist ..\build mkdir ..\build
 if not exist ..\build\SDL2.dll copy ..\third_party\SDL2\lib\%platform%\SDL2.dll ..\build
